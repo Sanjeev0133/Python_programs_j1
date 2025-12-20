6e9c3bcd27b783a1e20e9027d17a1f460b01bc23
Sanjeev0133 <itzsanqwerty@gmail.com>
2025-12-20 16:58:29 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types - in python 3 data types
+#1.int(integer)
+#2.str(string)
+#3.float(decimals)
+name="Sanjeev123"
+age=30
+salary=100000.98
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
